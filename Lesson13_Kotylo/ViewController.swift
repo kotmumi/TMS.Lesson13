a0836d734e3b934dd10372f17005b794f10814cd
Kotylo <kirillkotylo@gmail.com>
2025-04-13 20:07:40 +0300

    Initial Commit

---
+++ Lesson13_Kotylo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson13_Kotylo
+//
+//  Created by Кирилл Котыло on 13.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
